264af43fd013b800313fef42048b2245b9caa2b8
Shevchik Igor <s.hevchik.mail@gmail.com>
2024-11-26 06:49:08 +0300

    chore(struct): fix struct

--- build.config.ts
+++ build.config.ts
@@ -1,6 +1,11 @@
 import { defineBuildConfig } from 'unbuild'
 
 export default defineBuildConfig({
+  clean: true,
+  // @memo fix _1 ////
+  declaration: false,
+  // @memo fix _1 ////
+  failOnWarn: false,
   entries: [
     // Vue support
     './src/unplugin',
