25161102e06f631063a2ab1e6a3b6f20972683f3
Shevchik Igor <s.hevchik.mail@gmail.com>
2024-11-27 09:48:29 +0300

    style(Link): update

--- playground/app/pages/components/link.vue
+++ playground/app/pages/components/link.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="flex items-center gap-4">
-    <div class="flex flex-col items-start gap-2 text-sm">
+  <div class="flex items-center gap-6">
+    <div class="flex flex-col items-start gap-4 text-sm">
       <B24Link raw>
         Button raw
       </B24Link>
@@ -8,7 +8,7 @@
       <B24Link active>
         Button active
       </B24Link>
-      <B24Link active class="font-medium" active-class="text-[var(--ui-text-highlighted)]">
+      <B24Link active class="font-medium" active-class="text-alert-link">
         Button active with class
       </B24Link>
       <B24Link active disabled>
@@ -18,7 +18,7 @@
       <B24Link>
         Button inactive
       </B24Link>
-      <B24Link class="font-medium" inactive-class="hover:text-[var(--ui-primary)]">
+      <B24Link class="font-medium" inactive-class="hover:text-alert-link">
         Button inactive with class
       </B24Link>
       <B24Link disabled>
@@ -26,7 +26,7 @@
       </B24Link>
     </div>
 
-    <div class="flex flex-col items-start gap-2 text-sm">
+    <div class="flex flex-col items-start gap-4 text-sm">
       <B24Link to="/components/link" raw>
         Link raw
       </B24Link>
@@ -34,7 +34,7 @@
       <B24Link to="/components/link">
         Link active
       </B24Link>
-      <B24Link to="/components/link" class="font-medium" active-class="text-[var(--ui-text-highlighted)]">
+      <B24Link to="/components/link" class="font-medium" active-class="text-alert-link">
         Link active with class
       </B24Link>
       <B24Link to="/components/link" disabled>
@@ -44,7 +44,7 @@
       <B24Link to="/components/button">
         Link inactive
       </B24Link>
-      <B24Link to="/components/button" class="font-medium" inactive-class="hover:text-[var(--ui-primary)]">
+      <B24Link to="/components/button" class="font-medium" inactive-class="hover:text-alert-link">
         Link inactive with class
       </B24Link>
       <B24Link to="/components/button" disabled>
--- src/theme/link.ts
+++ src/theme/link.ts
@@ -2,14 +2,14 @@ import type { ModuleOptions } from '../module'
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export default (options: Required<ModuleOptions>) => ({
-  base: 'focus-visible:outline-primary-link',
+  base: 'cursor-pointer focus-visible:outline-primary-link',
   variants: {
     active: {
       true: 'text-primary-link',
-      false: ['text-base-500 hover:text-base-700', 'transition-colors']
+      false: ['text-base-500 hover:text-primary-link', 'transition-colors']
     },
     disabled: {
-      true: 'cursor-not-allowed opacity-75'
+      true: 'cursor-not-allowed opacity-75 hover:text-base-500'
     }
   }
 })
