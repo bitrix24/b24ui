3d936580a8bbe2acf7d94dbdafcd15aeaf8da915
Shevchik Igor <s.hevchik.mail@gmail.com>
2024-11-29 12:50:40 +0300

    docs(theme): make components public

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -3,6 +3,10 @@ import { h } from 'vue'
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import './tailwind.post.css'
+import ComponentExample from '~/.vitepress/theme/components/ui/ComponentExample.vue'
+import ComponentProps from '~/.vitepress/theme/components/ui/ComponentProps.vue'
+import ComponentSlots from '~/.vitepress/theme/components/ui/ComponentSlots.vue'
+import Description from '~/.vitepress/theme/components/ui/Description.vue'
 
 export default {
   extends: DefaultTheme,
@@ -11,7 +15,10 @@ export default {
       // https://vitepress.dev/guide/extending-default-theme#layout-slots
     })
   },
-  enhanceApp() {
-    // ...
+  enhanceApp({ app }) {
+    app.component('Description', Description)
+      .component('ComponentExample', ComponentExample)
+      .component('ComponentProps', ComponentProps)
+      .component('ComponentSlots', ComponentSlots)
   }
 } satisfies Theme
