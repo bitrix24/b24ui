ed6186ae258c0e25c68169a21041dabdefa5ed50
Shevchik Igor <s.hevchik.mail@gmail.com>
2024-12-02 06:49:16 +0300

    chore(deps): fix structure

--- docs/.vitepress/theme/components/ui/ExampleLoadingComponent.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import BtnClockIcon from '@bitrix24/b24icons-vue/button-specialized/BtnClockIcon'
-</script>
-
-<template>
-  <div class="scale-[4]">
-    <BtnClockIcon class="text-gray-150" />
-  </div>
-</template>
