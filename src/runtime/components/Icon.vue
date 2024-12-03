ce29b4677f94a0521fb57fea5c6789254a46175b
Shevchik Igor <s.hevchik.mail@gmail.com>
2024-12-03 16:58:34 +0300

    feat(icon): add icon struct

---
+++ src/runtime/components/Icon.vue
@@ -0,0 +1,27 @@
+<script lang="ts">
+export interface IconProps {
+  name: string
+  mode?: 'svg' | 'css'
+  size?: string | number
+  customize?: (
+    content: string,
+    name?: string,
+    prefix?: string,
+    provider?: string
+  ) => string
+}
+</script>
+
+<script setup lang="ts">
+import { B24Icon } from '@bitrix24/b24icons-vue'
+import { useForwardProps } from 'radix-vue'
+import { reactivePick } from '@vueuse/core'
+
+const props = defineProps<IconProps>()
+
+const iconProps = useForwardProps(reactivePick(props, 'name', 'mode', 'size', 'customize'))
+</script>
+
+<template>
+  <B24Icon v-bind="iconProps" />
+</template>
