d99a33399145625821b135032057fa07b88cc8ba
Shevchik Igor <s.hevchik.mail@gmail.com>
2024-12-06 09:00:36 +0300

    feat(playground): added some components to show examples

---
+++ playground/app/components/ExampleCard.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const $props = withDefaults(defineProps<{
+  title: string
+}>(), {
+  title: 'Some example'
+})
+</script>
+
+<template>
+  <div class="p-4 bg-white rounded-lg">
+    <div class="text-h5 font-semibold text-base-900 mb-1 pb-2 border-b-1 border-base-200">
+      [{{ $props.title }}]
+    </div>
+    <slot />
+  </div>
+</template>
---
+++ playground/app/components/ExampleCardSubTitle.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const $props = withDefaults(defineProps<{
+  title: string
+}>(), {
+  title: 'Some line'
+})
+</script>
+
+<template>
+  <div class="text-h6 text-base-200 mb-1">
+    -- {{ $props.title }} --
+  </div>
+</template>
