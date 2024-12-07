ed3e060f1d9b425a5d7698dd2ae2b0991b9f3259
Shevchik Igor <s.hevchik.mail@gmail.com>
2024-12-07 10:50:18 +0300

    chore(deps): code style

--- playground/app/pages/components/link.vue
+++ playground/app/pages/components/link.vue
@@ -1,61 +1,77 @@
 <script setup lang="ts">
 import usePageMeta from './../../composables/usePageMeta'
+import ExampleGrid from "~/components/ExampleGrid.vue";
+import ExampleCard from "~/components/ExampleCard.vue";
+import ExampleCardSubTitle from "~/components/ExampleCardSubTitle.vue";
 
 usePageMeta.setPageTitle('Links')
 </script>
 
 <template>
-  <div class="flex items-center gap-6">
-    <div class="flex flex-col items-start gap-4 text-sm">
-      <B24Link raw>
-        Button raw
-      </B24Link>
-
-      <B24Link active>
-        Button active
-      </B24Link>
-      <B24Link active class="font-medium" active-class="text-alert-link">
-        Button active with class
-      </B24Link>
-      <B24Link active disabled>
-        Button active disabled
-      </B24Link>
-
-      <B24Link>
-        Button inactive
-      </B24Link>
-      <B24Link class="font-medium" inactive-class="hover:not-disabled:not-aria-disabled:text-alert-link">
-        Button inactive with class
-      </B24Link>
-      <B24Link disabled>
-        Button inactive disabled
-      </B24Link>
-    </div>
-
-    <div class="flex flex-col items-start gap-4 text-sm">
-      <B24Link to="/components/link" raw>
-        Link raw
-      </B24Link>
-
-      <B24Link to="/components/link">
-        Link active
-      </B24Link>
-      <B24Link to="/components/link" class="font-medium" active-class="text-alert-link">
-        Link active with class
-      </B24Link>
-      <B24Link to="/components/link" disabled>
-        Link active disabled
-      </B24Link>
-
-      <B24Link to="/components/button">
-        Link inactive
-      </B24Link>
-      <B24Link to="/components/button" class="font-medium" inactive-class="hover:not-disabled:not-aria-disabled:text-alert-link">
-        Link inactive with class
-      </B24Link>
-      <B24Link to="/components/button" disabled>
-        Link inactive disabled
-      </B24Link>
-    </div>
-  </div>
+  <ExampleGrid>
+    <ExampleCard title="Button">
+      <ExampleCardSubTitle title="raw" />
+      <div class="flex flex-col items-start gap-4 text-sm">
+        <B24Link raw>
+          Button raw
+        </B24Link>
+      </div>
+      <ExampleCardSubTitle title="active" />
+      <div class="flex flex-col items-start gap-4 text-sm">
+        <B24Link active>
+          Button active
+        </B24Link>
+        <B24Link active class="font-medium" active-class="text-alert-link">
+          Button active with class
+        </B24Link>
+        <B24Link active disabled>
+          Button active disabled
+        </B24Link>
+      </div>
+      <ExampleCardSubTitle title="inactive" />
+      <div class="flex flex-col items-start gap-4 text-sm">
+        <B24Link>
+          Button inactive
+        </B24Link>
+        <B24Link class="font-medium" inactive-class="hover:not-disabled:not-aria-disabled:text-alert-link">
+          Button inactive with class
+        </B24Link>
+        <B24Link disabled>
+          Button inactive disabled
+        </B24Link>
+      </div>
+    </ExampleCard>
+    <ExampleCard title="B24Link">
+      <ExampleCardSubTitle title="raw" />
+      <div class="flex flex-col items-start gap-4 text-sm">
+        <B24Link to="/components/link" raw>
+          Link raw
+        </B24Link>
+      </div>
+      <ExampleCardSubTitle title="active" />
+      <div class="flex flex-col items-start gap-4 text-sm">
+        <B24Link to="/components/link">
+          Link active
+        </B24Link>
+        <B24Link to="/components/link" class="font-medium" active-class="text-alert-link">
+          Link active with class
+        </B24Link>
+        <B24Link to="/components/link" disabled>
+          Link active disabled
+        </B24Link>
+      </div>
+      <ExampleCardSubTitle title="inactive" />
+      <div class="flex flex-col items-start gap-4 text-sm">
+        <B24Link to="/components/button">
+          Link inactive
+        </B24Link>
+        <B24Link to="/components/button" class="font-medium" inactive-class="hover:not-disabled:not-aria-disabled:text-alert-link">
+          Link inactive with class
+        </B24Link>
+        <B24Link to="/components/button" disabled>
+          Link inactive disabled
+        </B24Link>
+      </div>
+    </ExampleCard>
+  </ExampleGrid>
 </template>
