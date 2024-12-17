52ed2c0fac025504f5abdc560d13adda6f624aca
Shevchik Igor <s.hevchik.mail@gmail.com>
2024-12-17 07:16:14 +0300

    fix(unplugin): invalid url schema on windows

--- src/unplugin.ts
+++ src/unplugin.ts
@@ -1,19 +1,15 @@
 import { fileURLToPath } from 'node:url'
 
-import { join } from 'pathe'
+import { join, normalize } from 'pathe'
 import { createUnplugin } from 'unplugin'
 import AutoImport from 'unplugin-auto-import'
 import { defu } from 'defu'
 import tailwind from '@tailwindcss/vite'
-// @todo remove this
-// import type colors from 'tailwindcss/colors'
 
 import type * as b24ui from '#build/b24ui'
 
 import { defaultOptions, getDefaultUiConfig } from './defaults'
 import type { ModuleOptions } from './module'
-// @todo remove this
-// import type icons from './theme/icons'
 
 import TemplatePlugin from './plugins/templates'
 import PluginsPlugin from './plugins/plugins'
@@ -22,10 +18,6 @@ import ComponentImportPlugin from './plugins/components'
 import Bitrix24EnvironmentPlugin from './plugins/bitrix24-environment'
 
 import type { DeepPartial } from './runtime/types/utils'
-
-// @todo remove this
-// type NeutralColor = 'slate' | 'gray' | 'zinc' | 'neutral' | 'stone'
-// type Color = Exclude<keyof typeof colors, 'inherit' | 'current' | 'transparent' | 'black' | 'white' | NeutralColor> | (string & {})
 
 type AppConfigB24UI = {} & DeepPartial<typeof b24ui>
 
@@ -40,7 +32,7 @@ export interface Bitrix24UIOptions extends Omit<ModuleOptions, 'colorMode'> {
   colorMode?: boolean
 }
 
-export const runtimeDir = fileURLToPath(new URL('./runtime', import.meta.url))
+export const runtimeDir = normalize(fileURLToPath(new URL('./runtime', import.meta.url)))
 
 export const Bitrix24UIPlugin = createUnplugin<Bitrix24UIOptions | undefined>((_options = {}, meta) => {
   const options = defu(_options, { fonts: false, devtools: { enabled: false } }, defaultOptions)
