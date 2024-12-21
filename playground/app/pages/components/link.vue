edbf2e97c868c4994018c260d645ac59a6798789
Shevchik Igor <s.hevchik.mail@gmail.com>
2024-12-21 07:19:04 +0300

    chore(playground): improved

--- playground/app/pages/components/link.vue
+++ playground/app/pages/components/link.vue
@@ -41,7 +41,7 @@ usePageMeta.setPageTitle('Links')
         </B24Link>
       </div>
     </ExampleCard>
-    <ExampleCard title="B24Link">
+    <ExampleCard title="Link">
       <ExampleCardSubTitle title="raw" />
       <div class="mb-4 flex flex-col items-start gap-4 text-sm">
         <B24Link to="/components/link" raw>
