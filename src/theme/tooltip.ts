b591cc92eda54f2551e80e8026bc79d2f729a917
IgorShevchik <s.hevchik.mail@gmail.com>
2024-12-22 08:58:41 +0300

    chore(Tooltip): improve

--- src/theme/tooltip.ts
+++ src/theme/tooltip.ts
@@ -12,9 +12,9 @@ export default {
       'flex items-center gap-1 shadow-sm rounded-2xs select-none data-[state=delayed-open]:animate-[scale-in_100ms_ease-out] data-[state=closed]:animate-[scale-out_100ms_ease-in]',
       'min-h-6 px-2 py-1 text-xs',
       'bg-base-dark/85 text-white',
-      'dark:bg-base-900/85 dark:text-base-200'
+      'dark:bg-dark dark:text-base-100 dark:ring dark:ring-base-800'
     ],
-    arrow: 'fill-base-dark dark:fill-base-900/85',
+    arrow: 'fill-base-dark dark:fill-base-800',
     text: 'text-pretty max-w-[200px]', // truncate
     kbds: `hidden lg:inline-flex items-center shrink-0 gap-0.5 before:content-[''] before:me-0.5`,
     kbdsSize: 'sm',
