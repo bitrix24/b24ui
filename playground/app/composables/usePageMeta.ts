515251f5aef3d3600e6e5ea8df62f02db2cc21c8
Shevchik Igor <s.hevchik.mail@gmail.com>
2024-12-25 07:59:39 +0300

    chore(playground): improve

--- playground/app/composables/usePageMeta.ts
+++ playground/app/composables/usePageMeta.ts
@@ -33,24 +33,24 @@ const menuList: IMenuItem[] = [
 
 const components: IComponentInfo[] = [
   { id: 'avatar', description: 'An img element with fallback and Nuxt Image support.', icon: PersonIcon },
-  { id: 'container', description: 'A container lets you center and constrain the width of your content.', icon: GroupIcon },
-  { id: 'separator', description: 'Separates content horizontally or vertically.', icon: GroupIcon },
-  { id: 'progress', description: 'An indicator showing the progress of a task.', icon: GroupIcon },
-  { id: 'link', description: 'A wrapper around NuxtLink with extra props.', icon: GroupIcon },
-  { id: 'tooltip', description: 'A popup that reveals information when hovering over an element.', icon: GroupIcon },
   { id: 'button', description: 'A button element that can act as a link or trigger an action.', icon: GroupIcon },
   { id: 'button-group', description: 'Group multiple button-like elements together.', icon: GroupIcon },
-  { id: 'input', description: 'An input element to enter text.', icon: FormIcon },
-  { id: 'textarea', description: 'A textarea element to input multi-line text.', icon: FormIcon },
-  { id: 'switch', description: 'A control that toggles between two states.', icon: FormIcon },
   { id: 'checkbox', description: 'An input element to toggle between checked and unchecked states.', icon: FormIcon },
-  { id: 'radio-group', description: 'A set of radio buttons to select a single option from a list.', icon: FormIcon },
-  { id: 'form-field', description: 'A wrapper for form elements that provides validation and error handling.', icon: FormIcon },
-  { id: 'range', description: 'An input to select a numeric value within a range.', icon: FormIcon },
   { id: 'chip', description: 'An indicator of a numeric value or a state.', icon: ItemIcon, iconUi: 'pr-px' },
+  { id: 'container', description: 'A container lets you center and constrain the width of your content.', icon: GroupIcon },
+  { id: 'form-field', description: 'A wrapper for form elements that provides validation and error handling.', icon: FormIcon },
+  { id: 'input', description: 'An input element to enter text.', icon: FormIcon },
   { id: 'kbd', description: 'A kbd element to display a keyboard key.', icon: ItemIcon, iconUi: 'pr-px' },
+  { id: 'link', description: 'A wrapper around NuxtLink with extra props.', icon: GroupIcon },
+  { id: 'progress', description: 'An indicator showing the progress of a task.', icon: GroupIcon },
+  { id: 'radio-group', description: 'A set of radio buttons to select a single option from a list.', icon: FormIcon },
+  { id: 'range', description: 'An input to select a numeric value within a range.', icon: FormIcon },
+  { id: 'separator', description: 'Separates content horizontally or vertically.', icon: GroupIcon },
   { id: 'shortcuts', description: 'A composable to define keyboard shortcuts in your app.', icon: ItemIcon, iconUi: 'pr-px' },
-  { id: 'skeleton', description: 'A placeholder to show while content is loading.', icon: ItemIcon, iconUi: 'pr-px' }
+  { id: 'skeleton', description: 'A placeholder to show while content is loading.', icon: ItemIcon, iconUi: 'pr-px' },
+  { id: 'switch', description: 'A control that toggles between two states.', icon: FormIcon },
+  { id: 'textarea', description: 'A textarea element to input multi-line text.', icon: FormIcon },
+  { id: 'tooltip', description: 'A popup that reveals information when hovering over an element.', icon: GroupIcon }
 ]
 
 const groups: IPageGroup[] = [
