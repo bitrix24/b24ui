fa885ef0f6ee4d4fd2dc31c6bcd5472f3750512d
Shevchik Igor <s.hevchik.mail@gmail.com>
2024-12-30 10:00:52 +0300

    chore(ButtonGroup/Button): Optimized behavior

--- src/theme/button-group.ts
+++ src/theme/button-group.ts
@@ -1,10 +1,16 @@
 export const buttonGroupVariant = {
   buttonGroup: {
     horizontal: [
+      'focus-visible:outline-none',
+      'ring ring-inset ring-0 focus-visible:ring-2',
       'not-only:first:rounded-e-none not-only:last:rounded-s-none not-last:not-first:rounded-none',
       'not-only:first:border-e-0 not-only:not-first:border-s-0'
     ].join(' '),
-    vertical: 'not-only:first:rounded-b-none not-only:last:rounded-t-none not-last:not-first:rounded-none'
+    vertical: [
+      'focus-visible:outline-none',
+      'ring ring-inset ring-0 focus-visible:ring-2',
+      'not-only:first:rounded-b-none not-only:last:rounded-t-none not-last:not-first:rounded-none'
+    ].join(' ')
   },
   noSplit: {
     false: [
@@ -20,13 +26,19 @@ export const buttonGroupVariantWithRoot = {
     horizontal: {
       root: 'group',
       base: [
+        'focus-visible:outline-none',
+        'ring ring-inset ring-1 focus-visible:ring-2',
         'group-not-only:group-first:rounded-e-none group-not-only:group-last:rounded-s-none group-not-last:group-not-first:rounded-none',
         'group-not-only:group-first:border-e-0 group-not-only:group-not-first:border-s-0'
       ].join(' ')
     },
     vertical: {
       root: 'group',
-      base: 'group-not-only:group-first:rounded-b-none group-not-only:group-last:rounded-t-none group-not-last:group-not-first:rounded-none'
+      base: [
+        'focus-visible:outline-none',
+        'ring ring-inset ring-1 focus-visible:ring-2',
+        'group-not-only:group-first:rounded-b-none group-not-only:group-last:rounded-t-none group-not-last:group-not-first:rounded-none'
+      ].join(' ')
     }
   },
   noSplit: {
