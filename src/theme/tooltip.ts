c40906ed720b29516d1789cb35263617295dffaa
Shevchik Igor <s.hevchik.mail@gmail.com>
2025-01-29 09:16:03 +0300

    fix(Tooltip): bg-color

--- src/theme/tooltip.ts
+++ src/theme/tooltip.ts
@@ -6,12 +6,15 @@
  * @see bitrix/js/ui/hint/
  */
 
+/**
+ * @memo bg-base-dark/85 - has some problem at render by tw -> fix to bg-base-dark
+ */
 export default {
   slots: {
     content: [
       'flex items-center gap-1 shadow-sm rounded-2xs select-none data-[state=delayed-open]:animate-[scale-in_100ms_ease-out] data-[state=closed]:animate-[scale-out_100ms_ease-in]',
       'min-h-6 px-2 py-1 text-xs',
-      'bg-base-dark/85 text-white',
+      'bg-base-dark text-white',
       'dark:bg-base-dark dark:text-base-150 dark:ring dark:ring-base-100/20',
       'pointer-events-auto'
     ],
