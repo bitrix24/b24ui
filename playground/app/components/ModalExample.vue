6706c3d3ba7534662bc6d6f831ba97c2ce949f04
Shevchik Igor <s.hevchik.mail@gmail.com>
2025-02-09 09:56:53 +0300

    chore(Modal): improve

--- playground/app/components/ModalExample.vue
+++ playground/app/components/ModalExample.vue
@@ -9,7 +9,10 @@ defineProps<{
 <template>
   <B24Modal :title="`This modal was opened programmatically ${count} times`">
     <template #footer>
-      <B24Button color="success" rounded label="Close" @click="modal.close()" />
+      <B24Button color="success" rounded label="Close" size="sm" @click="modal.close()" />
+      <B24ModalDialogClose>
+        <B24Button rounded label="Cancel" color="link" depth="dark" size="sm" />
+      </B24ModalDialogClose>
     </template>
   </B24Modal>
 </template>
--- src/theme/modal.ts
+++ src/theme/modal.ts
@@ -5,10 +5,8 @@
  * @link: /api_d7/bitrix/ui/dialogs/dialogs.php
  * @see bitrix/js/ui/dialogs/messagebox/..
  *
- * @todo: use modal template from - What's new in Bitrix24
- * @todo: set position ?? -> top / right
- * @todo: btn:link/normal - padding ?? remove -> see :b24ui="{ base: 'ps-0 pe-0' }"
- * @todo: how close modal from btn ??
+ * @todo: use modal template from - What's new in Bitrix24 + set position ~> top-right
+ * @todo: add wizard component
  */
 /*
   BX.UI.Dialogs.MessageBox.alert("Message", "Title", (messageBox, button, event) => {}, "OK");
@@ -19,15 +17,15 @@ export default {
     content: [
       'py-md2 px-5',
       'fixed w-full h-dvh bg-white dark:bg-base-950',
-      // 'divide-y divide-base-30 dark:divide-white/10',
+      // 'divide-y divide-base-900/10 dark:divide-white/20',
       'flex flex-col focus:outline-none'
     ].join(' '),
     header: 'pe-5 pt-0',
     body: 'flex-1 overflow-y-auto my-2.5 text-md leading-normal',
     footer: 'flex items-center justify-center gap-3 mt-2.5 pt-4 border-t border-t-1 border-t-base-900/10 dark:border-t-white/20',
-    title: 'font-bold text-md leading-normal text-black dark:text-base-150',
+    title: 'font-bold text-md leading-normal text-base-900 dark:text-base-150',
     description: 'mt-0.5 mb-1 text-base-500 dark:text-base-400 text-sm',
-    close: 'absolute top-2 end-1.5'
+    close: 'absolute top-2 end-1.5 p-0.5'
   },
   variants: {
     transition: {
@@ -43,7 +41,8 @@ export default {
       false: {
         content: [
           'top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]',
-          'sm:max-w-[512px] sm:h-auto sm:max-h-[calc(100vh-4rem)]',
+          'sm:min-w-[400px]',
+          'sm:max-w-[420px] sm:h-auto sm:max-h-[calc(100vh-4rem)]',
           'sm:rounded-md sm:shadow-lg',
           'sm:ring ring-base-300 dark:ring-base-800'
         ].join(' ')
