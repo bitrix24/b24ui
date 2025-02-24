2f551886e68df1001860e430c880f21a65921f41
Shevchik Igor <s.hevchik.mail@gmail.com>
2025-02-24 10:04:43 +0300

    chore(docs): improve toast example

--- docs/.vitepress/theme/components/ui/Layout.vue
+++ docs/.vitepress/theme/components/ui/Layout.vue
@@ -1,10 +1,19 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import VPLayout from 'vitepress/dist/client/theme-default/Layout.vue'
 import B24App from '@bitrix24/b24ui-nuxt/runtime/components/App.vue'
+
+const toaster = ref({
+  position: 'top-right' as const,
+  expand: true,
+  duration: 8000
+})
 </script>
 
 <template>
-  <B24App>
+  <B24App
+    :toaster="toaster"
+  >
     <VPLayout />
   </B24App>
 </template>
