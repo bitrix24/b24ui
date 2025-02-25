c4c916731d71b4c2c6c370b0882be671ee270d15
Shevchik Igor <s.hevchik.mail@gmail.com>
2025-02-25 07:24:29 +0300

    chore(docs): improve app

--- docs/.vitepress/theme/components/ui/Layout.vue
+++ docs/.vitepress/theme/components/ui/Layout.vue
@@ -1,19 +1,9 @@
 <script setup lang="ts">
-import { ref } from 'vue'
 import VPLayout from 'vitepress/dist/client/theme-default/Layout.vue'
-import B24App from '@bitrix24/b24ui-nuxt/runtime/components/App.vue'
-
-const toaster = ref({
-  position: 'top-right' as const,
-  expand: true,
-  duration: 8000
-})
 </script>
 
 <template>
-  <B24App
-    :toaster="toaster"
-  >
+  <B24App>
     <VPLayout />
   </B24App>
 </template>
