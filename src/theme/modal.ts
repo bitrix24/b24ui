c84733662746bf8cb91bc05f61d5055808c7591c
Shevchik Igor <s.hevchik.mail@gmail.com>
2025-02-25 09:03:36 +0300

    fix(Modal): header min-height

--- src/theme/modal.ts
+++ src/theme/modal.ts
@@ -20,8 +20,8 @@ export default {
       'fixed bg-white dark:bg-base-950',
       'flex flex-col focus:outline-none'
     ].join(' '),
-    header: 'flex items-center gap-1.5 pe-5 pt-0 min-h-lg2',
-    wrapper: '',
+    header: 'flex items-center gap-1.5 pe-5 pt-0',
+    wrapper: 'min-h-lg2',
     body: 'flex-1 overflow-y-auto my-2.5 text-md leading-normal',
     footer: 'flex items-center justify-center gap-3 mt-2.5 pt-4 border-t border-t-1 border-t-base-900/10 dark:border-t-white/20',
     title: 'font-bold text-md leading-normal text-base-900 dark:text-base-150',
