ab66d7b1a73cd47eccb5cee0b6f852f711600b4d
Shevchik Igor <s.hevchik.mail@gmail.com>
2025-04-10 08:14:57 +0300

    docs(form): improve types

--- docs/examples/form/FormExample.vue
+++ docs/examples/form/FormExample.vue
@@ -31,7 +31,7 @@ const validate = (state: any): FormError[] => {
 }
 
 const toast = useToast()
-async function onSubmit(event: FormSubmitEvent<Schema>) {
+async function onSubmit(event: FormSubmitEvent<typeof state>) {
   toast.add({ title: 'Success', description: 'The form has been submitted.', color: 'success' })
   console.log(event.data, state)
   isShowResult.value = true
