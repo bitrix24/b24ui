61c38f6f3b86f8f85d8275c2324df4a7db2cb19e
Shevchik Igor <s.hevchik.mail@gmail.com>
2025-04-22 08:47:17 +0300

    fix(App): fix server side for `portal` prop

--- src/runtime/composables/usePortal.ts
+++ src/runtime/composables/usePortal.ts
@@ -4,8 +4,23 @@ export const portalTargetInjectionKey: InjectionKey<Ref<string | HTMLElement>> =
 
 export function usePortal(portal: Ref<string | HTMLElement | boolean | undefined>) {
   const portalTarget = inject(portalTargetInjectionKey, undefined)
+  /**
+   * @memo sh: fix server side
+   */
+  const isClient = typeof window !== 'undefined'
 
   const to = computed(() => {
+    /**
+     * @memo sh: fix server side
+     */
+    if (!isClient) {
+      if (typeof portal.value === 'string') {
+        return portal.value
+      }
+
+      return portalTarget?.value ?? 'body'
+    }
+
     if (typeof portal.value === 'string' || portal.value instanceof HTMLElement) {
       return portal.value
     }
