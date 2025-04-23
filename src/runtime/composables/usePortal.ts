03ff0ecb9478dc845017b71e3cd005122fc6024a
Shevchik Igor <s.hevchik.mail@gmail.com>
2025-04-23 07:13:28 +0300

    fix(usePortal): adjust portal target resolution logic

--- src/runtime/composables/usePortal.ts
+++ src/runtime/composables/usePortal.ts
@@ -4,28 +4,13 @@ export const portalTargetInjectionKey: InjectionKey<Ref<string | HTMLElement>> =
 
 export function usePortal(portal: Ref<string | HTMLElement | boolean | undefined>) {
   const portalTarget = inject(portalTargetInjectionKey, undefined)
-  /**
-   * @memo sh: fix server side
-   */
-  const isClient = typeof window !== 'undefined'
 
   const to = computed(() => {
-    /**
-     * @memo sh: fix server side
-     */
-    if (!isClient) {
-      if (typeof portal.value === 'string') {
-        return portal.value
-      }
-
+    if (typeof portal.value === 'boolean' || portal.value === undefined) {
       return portalTarget?.value ?? 'body'
     }
 
-    if (typeof portal.value === 'string' || portal.value instanceof HTMLElement) {
-      return portal.value
-    }
-
-    return portalTarget?.value ?? 'body'
+    return portal.value
   })
 
   const disabled = computed(() => typeof portal.value === 'boolean' ? !portal.value : false)
