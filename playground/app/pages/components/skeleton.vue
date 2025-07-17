6c6a16c4e0a02dba3d731679c7f7153631cde7f1
Shevchik Igor <s.hevchik.mail@gmail.com>
2025-07-17 10:59:36 +0300

    chore(AirWeb): Skeleton improve

--- playground/app/pages/components/skeleton.vue
+++ playground/app/pages/components/skeleton.vue
@@ -19,13 +19,22 @@ usePageMeta.setPageTitle('Skeleton')
         </div>
       </div>
 
-      <ExampleCardSubTitle title="rtl" />
+      <ExampleCardSubTitle title="rtl & accent" />
       <div class="mb-4 w-full flex items-center justify-end gap-4">
         <div class="space-y-2 flex flex-col items-end">
-          <B24Skeleton class="h-4 w-[150px]" />
-          <B24Skeleton class="h-4 w-[100px]" />
+          <B24Skeleton accent="accent" class="h-4 w-[150px]" />
+          <B24Skeleton accent="accent" class="h-4 w-[100px]" />
         </div>
-        <B24Skeleton class="h-12 w-12 rounded-full" />
+        <B24Skeleton accent="accent" class="h-12 w-12 rounded-full" />
+      </div>
+
+      <ExampleCardSubTitle title="less" />
+      <div class="mb-8 w-full flex items-center gap-4">
+        <B24Skeleton accent="less" class="h-12 w-12 rounded-full" />
+        <div class="space-y-2">
+          <B24Skeleton accent="less" class="h-4 w-[150px]" />
+          <B24Skeleton accent="less" class="h-4 w-[100px]" />
+        </div>
       </div>
     </ExampleCard>
   </ExampleGrid>
