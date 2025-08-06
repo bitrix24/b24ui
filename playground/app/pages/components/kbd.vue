b25257a120bdde270581bee52e6e8b3b50c5e8e1
IgorShevchik <s.hevchik.mail@gmail.com>
2025-08-06 14:04:03 +0300

    fix(Kbd): colors

--- playground/app/pages/components/kbd.vue
+++ playground/app/pages/components/kbd.vue
@@ -20,7 +20,7 @@ const kbdKeys = Object.keys(kbdKeysMap)
         <B24Kbd value="meta" accent="less" />
         <B24Kbd value="meta" accent="default" />
         <B24Kbd value="meta" accent="accent" />
-        <B24Kbd value="meta" accent="default" class="air-primary" />
+        <B24Kbd value="meta" accent="default" class="style-filled" />
       </div>
       <ExampleCardSubTitle title="size" />
       <div class="mb-4 flex flex-wrap items-center justify-start gap-4">
