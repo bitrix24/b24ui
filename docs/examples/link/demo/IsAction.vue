07a1b02125a2c15bc640eb151f1b2dbb4ee556e0
Shevchik Igor <s.hevchik.mail@gmail.com>
2025-08-22 15:37:21 +0300

    docs(Link) improve

--- docs/examples/link/demo/IsAction.vue
+++ docs/examples/link/demo/IsAction.vue
@@ -1,5 +1,5 @@
 <template>
-  <B24Link is-action to="b24ui/components/link.html">
+  <B24Link is-action to="https://bitrix24.github.io/b24ui/components/link.html" target="_blank">
     Link action
   </B24Link>
   <B24Link is-action>
--- docs/examples/link/demo/LinkTo.vue
+++ docs/examples/link/demo/LinkTo.vue
@@ -1,8 +1,8 @@
 <template>
-  <B24Link to="b24ui/components/link.html" active>
+  <B24Link to="https://bitrix24.github.io/b24ui/components/link.html" active target="_blank">
     Link (active)
   </B24Link>
-  <B24Link to="wrong_path">
+  <B24Link to="wrong_path" target="_blank">
     Link (not active)
   </B24Link>
 </template>
