35a827b6289d4d9f4691eff2b1bd88bd5a13eb08
Shevchik Igor <s.hevchik.mail@gmail.com>
2025-09-15 09:36:41 +0300

    fix(CodeCollapse): improve theme

--- src/theme/prose/code-collapse.ts
+++ src/theme/prose/code-collapse.ts
@@ -20,7 +20,7 @@ export default {
       },
       false: {
         root: '[&_pre]:overflow-hidden',
-        footer: 'bg-gradient-to-t from-(--b24ui-typography-label-color)'
+        footer: 'bg-gradient-to-t from-(--ui-color-gray-04)'
       }
     }
   }
