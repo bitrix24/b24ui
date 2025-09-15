f1ec45560efb2edbbae0ed870dccb7b76b03d86d
Shevchik Igor <s.hevchik.mail@gmail.com>
2025-09-15 09:36:52 +0300

    fix(ProsePre): improve theme

--- src/theme/prose/pre.ts
+++ src/theme/prose/pre.ts
@@ -6,7 +6,6 @@
 export default {
   slots: {
     root: [
-      'style-tinted-no-accent-1',
       'relative',
       'group',
       'my-5',
@@ -14,14 +13,14 @@ export default {
     ].join(' '),
     header: [
       'flex items-center gap-1.5',
-      'border-(--b24ui-border-color) border-(length:--b24ui-border-width)',
-      'bg-(--b24ui-background)/20',
+      'border-(--ui-color-divider-vibrant-accent-more) border',
+      'bg-(--ui-color-gray-01)',
       'border-b-0 relative',
       'rounded-t-md',
       'px-4 py-3'
     ].join(' '),
-    filename: 'text-(--b24ui-color) text-(length:--ui-font-size-md)/(--ui-font-line-height-md)',
-    icon: 'size-4 shrink-0',
+    filename: 'text-(--ui-color-gray-70) text-(length:--ui-font-size-md)/(--ui-size-xl)',
+    icon: 'size-[16px] shrink-0 mt-px',
     copy: 'absolute top-[11px] right-[11px] lg:opacity-0 lg:group-hover:opacity-100 transition',
     base: [
       // 'text-green-350'
@@ -29,8 +28,8 @@ export default {
       'text-pretty',
       'text-(length:--ui-font-size-sm)/[normal]',
       'font-[family-name:var(--ui-font-family-system-mono)]',
-      'border-(--b24ui-border-color) border-(length:--b24ui-border-width)',
-      'bg-(--b24ui-background)',
+      'border-(--ui-color-divider-vibrant-accent-more) border',
+      'bg-(--ui-color-gray-04)',
       'rounded-(--ui-border-radius-md)',
       'px-4 py-3',
       'whitespace-pre-wrap',
