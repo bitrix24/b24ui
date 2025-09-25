b9c92e86fa9ccb4ae1cdf5112742c5ecbe6b8fb7
Shevchik Igor <s.hevchik.mail@gmail.com>
2025-09-25 09:14:01 +0300

    docs(InputMenu): improve

--- docs/app/components/content/examples/input-menu/InputMenuSearchTermExample.vue
+++ docs/app/components/content/examples/input-menu/InputMenuSearchTermExample.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 const searchTerm = ref('o')
 const items = ref(['CRM settings', 'My company details', 'Access permissions'])
-const value = ref(undefined)
+const value = ref('My company details')
 </script>
 
 <template>
