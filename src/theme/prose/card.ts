b4fd0f0244d092072c0172ba5b4b13fbd141e0f3
Shevchik Igor <s.hevchik.mail@gmail.com>
2025-10-14 13:40:51 +0300

    chore(ProseCard): improve

--- src/theme/prose/card.ts
+++ src/theme/prose/card.ts
@@ -1,28 +1,46 @@
 /**
  * ProseCard
  * ---
- * @todo fix color
  */
 
 export default {
   slots: {
     base: [
-      'group relative block my-5 p-4 sm:p-6 border border-default rounded-md bg-default',
+      'group',
+      'relative block',
+      'my-5 p-4 sm:p-6',
+      'border border-(--ui-color-design-tinted-na-stroke)',
+      'bg-(--b24ui-background)',
+      'rounded-md',
       'transition-colors'
     ].join(' '),
-    icon: 'size-6 mb-2 block',
-    title: 'text-highlighted font-semibold',
-    description: 'text-[15px] text-(--b24ui-typography-description-color) *:first:mt-0 *:last:mb-0 *:my-1',
+    icon: 'size-6 mb-2 block text-(--b24ui-icon)',
+    title: 'text-(--b24ui-color) font-semibold',
+    description: 'text-[15px] text-(--b24ui-color) *:first:mt-0 *:last:mb-0 *:my-1',
     externalIcon: [
-      'size-4 align-top absolute right-2 top-2 text-dimmed pointer-events-none',
+      'size-4',
+      'align-top',
+      'absolute',
+      'right-2 top-2',
+      'pointer-events-none',
+      'text-(--b24ui-split-divider-color)',
       'transition-colors'
     ].join(' ')
   },
   variants: {
     color: {
-      'air-primary': {
-        icon: 'text-highlighted'
-      }
+      'air-primary': { base: 'style-filled' },
+      'air-primary-success': { base: 'style-filled-success' },
+      'air-primary-alert': { base: 'style-filled-alert' },
+      'air-primary-copilot': { base: 'style-filled-copilot' },
+      'air-primary-warning': { base: 'style-filled-warning' },
+      'air-secondary': { base: 'style-tinted' },
+      'air-secondary-alert': { base: 'style-tinted-alert' },
+      'air-secondary-accent': { base: 'style-tinted-no-accent-1' },
+      'air-secondary-accent-1': { base: 'style-outline-accent-1' },
+      'air-secondary-accent-2': { base: 'style-outline-accent-2' },
+      'air-secondary-no-accent': { base: 'style-outline' },
+      'air-tertiary': { base: 'style-outline-no-accent' }
     },
     to: {
       true: ''
@@ -35,11 +53,12 @@ export default {
   },
   compoundVariants: [
     {
-      color: 'air-primary',
       to: true,
       class: {
-        base: 'hover:bg-elevated/50 hover:border-inverted',
-        externalIcon: 'group-hover:text-highlighted'
+        base: 'hover:bg-(--ui-color-bg-content-secondary) hover:border-(--b24ui-border-color-hover)',
+        title: 'group-hover:text-(--b24ui-border-color)',
+        description: 'group-hover:text-(--b24ui-border-color)',
+        externalIcon: 'group-hover:text-(--b24ui-icon)'
       }
     }
   ],
