1f27bfb863e9d61e8b7d72c1c81cf5bb1f378111
Shevchik Igor <s.hevchik.mail@gmail.com>
2025-10-27 07:28:59 +0300

    docs(search): move ai assistant at the top

--- docs/app/components/search/Search.vue
+++ docs/app/components/search/Search.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import type { UIMessage } from 'ai'
 import type { ContentNavigationItem } from '@nuxt/content'
 // import RobotIcon from '@bitrix24/b24icons-vue/outline/RobotIcon'
 
@@ -16,51 +15,7 @@ defineProps<{
   navigation?: ContentNavigationItem[]
 }>()
 
-const chat = ref(false)
-const fullscreen = ref(false)
-const searchTerm = ref('')
-const messages = ref<UIMessage[]>([])
-
-const { frameworks } = useFrameworks()
-const { links } = useSearch()
-
-const groups = computed(() => [
-  // {
-  //   id: 'ai',
-  //   label: 'Assistant',
-  //   ignoreFilter: true,
-  //   items: [{
-  //     label: 'Ask Bitrix24 AI',
-  //     icon: RobotIcon,
-  //     ui: {
-  //       itemLeadingIcon: 'group-data-highlighted:not-group-data-disabled:text-(--ui-color-accent-main-primary)'
-  //     },
-  //     onSelect: (e: any) => {
-  //       e.preventDefault()
-  //
-  //       messages.value = searchTerm.value
-  //         ? [{
-  //             id: '1',
-  //             role: 'user',
-  //             parts: [{ type: 'text', text: searchTerm.value }]
-  //           }]
-  //         : []
-  //
-  //       chat.value = true
-  //     }
-  //   }]
-  // },
-  {
-    id: 'framework',
-    label: 'Framework',
-    items: frameworks.value
-  }
-])
-
-function onClose() {
-  chat.value = false
-  fullscreen.value = false
-}
+const { links, groups, fullscreen, searchTerm } = useSearch()
 </script>
 
 <template>
@@ -73,9 +28,5 @@ function onClose() {
     :fullscreen="fullscreen"
     :color-mode="false"
     :fuse="{ resultLimit: 115 }"
-  >
-    <template v-if="chat" #content>
-      <SearchChat v-model:messages="messages" v-model:fullscreen="fullscreen" @close="onClose" />
-    </template>
-  </B24ContentSearch>
+  />
 </template>
--- docs/app/composables/useSearch.ts
+++ docs/app/composables/useSearch.ts
@@ -1,15 +1,50 @@
+import type { UIMessage } from 'ai'
 import PlayLIcon from '@bitrix24/b24icons-vue/outline/PlayLIcon'
 import DeveloperResourcesIcon from '@bitrix24/b24icons-vue/outline/DeveloperResourcesIcon'
 import ViewmodeCodeIcon from '@bitrix24/b24icons-vue/editor/ViewmodeCodeIcon'
 import FormattingIcon from '@bitrix24/b24icons-vue/editor/FormattingIcon'
 // import FormIcon from '@bitrix24/b24icons-vue/outline/FormIcon'
 // import DemonstrationOnIcon from '@bitrix24/b24icons-vue/outline/DemonstrationOnIcon'
+// import RobotIcon from '@bitrix24/b24icons-vue/outline/RobotIcon'
 import GitHubIcon from '@bitrix24/b24icons-vue/social/GitHubIcon'
 
 export function useSearch() {
   const route = useRoute()
+  const { frameworks } = useFrameworks()
+
+  const chat = ref(false)
+  const fullscreen = ref(false)
+  const searchTerm = ref('')
+  const messages = ref<UIMessage[]>([])
+
+  // function onSelect(e: any) {
+  //   e.preventDefault()
+  //
+  //   messages.value = searchTerm.value
+  //     ? [{
+  //         id: '1',
+  //         role: 'user',
+  //         parts: [{ type: 'text', text: searchTerm.value }]
+  //       }]
+  //     : [{
+  //         id: '1',
+  //         role: 'assistant',
+  //         parts: [{ type: 'text', text: 'Hello, how can I help you today?' }]
+  //       }]
+  //
+  //   chat.value = true
+  // }
 
   const links = computed(() => [
+    // !searchTerm.value && {
+    //   label: 'Ask AI',
+    //   description: 'Ask the AI assistant powered by our custom MCP server for help.',
+    //   icon: RobotIcon,
+    //   b24ui: {
+    //     itemLeadingIcon: 'group-data-highlighted:not-group-data-disabled:text-(--ui-color-accent-main-primary)'
+    //   },
+    //   onSelect
+    // },
     {
       label: 'Docs',
       description: 'Learn how to get started with Bitrix24 UI.',
@@ -57,9 +92,37 @@ export function useSearch() {
       to: 'https://github.com/bitrix24/b24ui',
       target: '_blank'
     }
+  ].filter(link => !!link))
+
+  const groups = computed(() => [
+    // {
+    //   id: 'ai',
+    //   label: 'AI',
+    //   ignoreFilter: true,
+    //   items: searchTerm.value
+    //     ? [{
+    //         label: `Ask AI for “${searchTerm.value}”`,
+    //         icon: RobotIcon,
+    //         b24ui: {
+    //           itemLeadingIcon: 'group-data-highlighted:not-group-data-disabled:text-(--ui-color-accent-main-primary)'
+    //         },
+    //         onSelect
+    //       }]
+    //     : []
+    // },
+    {
+      id: 'framework',
+      label: 'Framework',
+      items: frameworks.value
+    }
   ])
 
   return {
-    links
+    links,
+    groups,
+    chat,
+    fullscreen,
+    searchTerm,
+    messages
   }
 }
