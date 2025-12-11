2b633e501cabab192ae2c312de13b3700e98780c
Shevchik Igor <s.hevchik.mail@gmail.com>
2025-12-11 07:14:48 +0300

    docs(toast): add callback example

---
+++ docs/app/components/content/examples/toast/ToastCallbackExample.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import UploadIcon from '@bitrix24/b24icons-vue/outline/UploadIcon'
+import CheckLIcon from '@bitrix24/b24icons-vue/outline/CheckLIcon'
+
+const toast = useToast()
+
+function showToast() {
+  toast.add({
+    'title': 'Uploading file...',
+    'description': 'Your file is being uploaded.',
+    'icon': UploadIcon,
+    'duration': 3000,
+    'onUpdate:open': (open) => {
+      if (!open) {
+        toast.add({
+          title: 'File uploaded!',
+          description: 'Your file has been successfully uploaded.',
+          icon: CheckLIcon,
+          color: 'air-primary-success'
+        })
+      }
+    }
+  })
+}
+</script>
+
+<template>
+  <B24Button label="Show toast" @click="showToast" />
+</template>
