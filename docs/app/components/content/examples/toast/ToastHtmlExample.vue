5bd167e8713ac37af5f7da6e6256304de62317cd
Shevchik Igor <s.hevchik.mail@gmail.com>
2026-02-12 07:05:54 +0300

    docs(toast): add HTML content example

---
+++ docs/app/components/content/examples/toast/ToastHtmlExample.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import TrashcanIcon from '@bitrix24/b24icons-vue/solid/TrashcanIcon'
+
+const toast = useToast()
+
+function showToast() {
+  toast.add({
+    title: h('span', {}, [
+      'Item ',
+      h('span', { class: 'text-(--ui-color-accent-main-primary) font-bold' }, '#15'),
+      ' deleted'
+    ]),
+    description: h('span', {}, [
+      'You have successfully deleted the item from your ',
+      h('span', { class: 'font-bold' }, 'account'),
+      '.'
+    ]),
+    icon: TrashcanIcon
+  })
+}
+</script>
+
+<template>
+  <B24Button label="Show toast" @click="showToast" />
+</template>
