d25b7fc1c5a5a378a1faf5a7189b905bdc0d6c34
IgorShevchik <s.hevchik.mail@gmail.com>
2026-02-15 09:07:42 +0300

    playground(vue): support allowedHosts

--- playgrounds/vue/vite.config.ts
+++ playgrounds/vue/vite.config.ts
@@ -1,21 +1,29 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import bitrix24UIPluginVite from '@bitrix24/b24ui-nuxt/vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    bitrix24UIPluginVite({
-      colorModeTypeLight: 'edge-dark',
-      b24ui: {},
-      autoImport: {
-        dirs: ['../nuxt/app/composables'],
-        imports: ['vue']
-      },
-      components: {
-        dirs: ['../nuxt/app/components']
-      }
-    })
-  ]
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+  const extraAllowedHosts = (env?.VITE_ALLOWED_HOSTS?.split(',').map((s: string) => s.trim()).filter(Boolean)) ?? []
+  return {
+    plugins: [
+      vue(),
+      bitrix24UIPluginVite({
+        colorModeTypeLight: 'edge-dark',
+        b24ui: {},
+        autoImport: {
+          dirs: ['../nuxt/app/composables'],
+          imports: ['vue']
+        },
+        components: {
+          dirs: ['../nuxt/app/components']
+        }
+      }),
+    ],
+    server: {
+      // Fix: "Blocked request. This host is not allowed" when using tunnels like ngrok
+      allowedHosts: [...extraAllowedHosts]
+    }
+  }
 })
