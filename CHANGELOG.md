2e260ebb02732ef18dd19ffada5a914bd501832a
Shevchik Igor <s.hevchik.mail@gmail.com>
2026-02-19 12:58:21 +0300

    chore(deps): update @nuxt/test-utils + vitest to v4

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -22,6 +22,7 @@
 * fix(Toast): allow `update` to keep toast open and reset duration
 * fix(InputMenu/SelectMenu): sort filtered items by match relevance
 * fix(components): nullable and optional type support
+* chore(deps): update @nuxt/test-utils + vitest to v4
 
 ## [2.3.0](https://github.com/bitrix24/b24ui/compare/v2.2.1...v2.3.0) (2026-02-12)
 
--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,6 @@ import { defineVitestProject } from '@nuxt/test-utils/config'
 import { defineConfig } from 'vitest/config'
 import vue from '@vitejs/plugin-vue'
 import bitrix24UIPluginVite from './src/vite'
-import { resolve } from 'pathe'
 import { glob } from 'tinyglobby'
 
 const components = await glob('./src/runtime/components/*.vue', { absolute: true })
@@ -27,7 +26,12 @@ export default defineConfig({
         extends: true,
         test: {
           name: 'nuxt',
-          include: ['./test/components/**/**.spec.ts', './test/composables/**.spec.ts', './test/utils/**/**.spec.ts'],
+          dir: './test',
+          include: [
+            'components/**/**.spec.ts',
+            'composables/**.spec.ts',
+            'utils/**/**.spec.ts'
+          ],
           environment: 'nuxt',
           environmentOptions: {
             nuxt: {
@@ -42,10 +46,11 @@ export default defineConfig({
         test: {
           name: 'vue',
           environment: 'happy-dom',
+          dir: './test',
           include: [
-            './test/components/**.spec.ts',
-            './test/composables/**.spec.ts',
-            './test/utils/**/**.spec.ts'
+            'components/**.spec.ts',
+            'composables/**.spec.ts',
+            'utils/**/**.spec.ts'
           ],
           setupFiles: ['./test/utils/setup.ts']
         },
@@ -57,7 +62,7 @@ export default defineConfig({
             enforce: 'pre',
             resolveId(id) {
               if (id === '@nuxt/test-utils/runtime') {
-                return resolve('./test/utils/mount')
+                return fileURLToPath(new URL('test/utils/mount.ts', import.meta.url))
               }
             }
           },
@@ -70,7 +75,7 @@ export default defineConfig({
               }
             },
             load(id) {
-              if (id === '#components' || id === '?#components') {
+              if (id === '#components' || id === '?import#components') {
                 const resolvedComponents = [...vueRouterOverrides, ...vueComponents, ...components]
                 const renderedComponents = new Set<string>()
                 return resolvedComponents.map((file) => {
