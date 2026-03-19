a1bbf21eb273d999e659f0c558189b0ca0c8f587
Shevchik Igor <s.hevchik.mail@gmail.com>
2026-03-19 11:02:28 +0300

    feat(DashboardSidebar): improve theme

--- src/theme/dashboard-panel.ts
+++ src/theme/dashboard-panel.ts
@@ -5,8 +5,8 @@
  */
 export default {
   slots: {
-    root: 'relative flex flex-col min-w-0 min-h-svh lg:not-last:border-e lg:not-last:border-(--ui-color-divider-default) shrink-0',
-    body: 'flex flex-col gap-4 sm:gap-6 flex-1 overflow-y-auto pt-2.5 lg:p-4 scrollbar-thin',
+    root: 'relative flex flex-col min-w-0 min-h-svh lg:not-last:border-e lg:not-last:border-e-0 lg:not-last:border-(--ui-color-divider-default) shrink-0',
+    body: 'flex flex-col gap-4 sm:gap-6 flex-1 overflow-y-auto sm:p-4 sm:pt-0 scrollbar-thin',
     handle: ''
   },
   variants: {
