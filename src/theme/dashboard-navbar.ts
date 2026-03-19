8d2ad6dc3b9f9dbff9f2e64f6d0752f73c1f6b57
Shevchik Igor <s.hevchik.mail@gmail.com>
2026-03-19 08:42:03 +0300

    feat(DashboardNavbar): improve theme

--- src/theme/dashboard-navbar.ts
+++ src/theme/dashboard-navbar.ts
@@ -5,12 +5,12 @@
  */
 export default {
   slots: {
-    root: 'h-(--b24ui-header-height) shrink-0 flex items-center justify-between border-b border-(--ui-color-divider-default) px-4 gap-1.5',
+    root: 'h-(--b24ui-header-height) shrink-0 flex items-center justify-between ps-2 pe-4 lg:ps-4 lg:pe-4 gap-1.5',
     left: 'flex items-center gap-1.5 min-w-0',
     icon: 'shrink-0 size-5 self-center me-1.5',
-    title: 'flex items-center gap-1.5 font-(--ui-font-weight-semi-bold) text-(--b24ui-typography-label-color) truncate',
+    title: 'flex items-center gap-1.5 text-[length:25px] bitrix-mobile:text-[length:18px] font-medium text-(--ui-color-base-1) truncate',
     center: 'hidden lg:flex',
-    right: 'flex items-center shrink-0 gap-1.5',
+    right: 'flex items-center shrink-0 gap-3',
     toggle: ''
   },
   variants: {
