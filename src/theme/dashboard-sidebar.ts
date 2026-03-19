bd7fbd17d1721584bda5ffc2454ffc5e1c7201e4
Shevchik Igor <s.hevchik.mail@gmail.com>
2026-03-19 10:26:16 +0300

    feat(DashboardSidebar): improve theme

--- src/theme/dashboard-sidebar.ts
+++ src/theme/dashboard-sidebar.ts
@@ -5,10 +5,18 @@
  */
 export default {
   slots: {
-    root: 'relative hidden lg:flex flex-col min-h-svh min-w-16 w-(--width) shrink-0',
-    header: 'h-(--b24ui-header-height) shrink-0 flex items-center gap-1.5 px-4',
-    body: 'flex flex-col gap-4 flex-1 overflow-y-auto py-2 scrollbar-thin scrollbar-transparent',
-    footer: 'shrink-0 flex items-center gap-1.5 px-4 py-2',
+    root: [
+      'relative hidden lg:flex flex-col min-h-svh min-w-16 w-(--width) shrink-0',
+      'backdrop-blur-sm bg-(--ui-color-bg-content-primary)/20 light:bg-transparent dark:bg-(--ui-color-bg-content-primary)/70',
+      'light:bg-transparent light:[--leftmenu-group-stroke:var(--ui-color-base-30)]'
+    ].join(' '),
+    header: 'h-(--b24ui-header-height) shrink-0 flex items-center gap-1.5 ps-4 pe-4',
+    body: 'flex flex-col gap-4 flex-1 overflow-y-auto ps-4 pe-4 py-2 scrollbar-thin scrollbar-transparent',
+    footer: [
+      'shrink-0 flex items-center gap-1.5 ps-4 pe-4 py-2',
+      'lg:border-t lg:border-(--ui-color-divider-default)',
+      'light:lg:border-(--ui-color-base-30)'
+    ].join(' '),
     toggle: '',
     handle: '',
     content: 'lg:hidden',
@@ -24,7 +32,7 @@ export default {
     },
     side: {
       left: {
-        root: 'border-e border-(--ui-color-divider-default)'
+        root: 'border-e border-e-0 border-(--ui-color-divider-accent)'
       },
       right: {
         root: ''
