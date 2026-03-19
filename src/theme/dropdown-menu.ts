701add1a23904fcdff00fe5c3fa4726f1d833cb2
Shevchik Igor <s.hevchik.mail@gmail.com>
2026-03-19 09:36:03 +0300

    feat(DropdownMenu): improve theme

--- src/theme/dropdown-menu.ts
+++ src/theme/dropdown-menu.ts
@@ -4,7 +4,6 @@
  * ---
  * @see src/theme/context-menu.ts
  * @see src/theme/editor-suggestion-menu.ts
- * @todo add Badge
  */
 
 export default {
@@ -14,57 +13,70 @@ export default {
       'bg-(--ui-color-bg-content-primary)',
       'shadow-(--popup-window-box-shadow)',
       'rounded-(--ui-border-radius-xl) will-change-[opacity]',
-      // 'ring ring-(--ui-color-divider-default)',
       'motion-safe:data-[state=open]:animate-[scale-in_100ms_ease-out] motion-safe:data-[state=closed]:animate-[scale-out_100ms_ease-in]',
       'origin-(--reka-dropdown-menu-content-transform-origin)',
       'font-[family-name:var(--ui-font-family-primary)]',
       'relative',
       'isolate',
-      'px-0 py-(--menu-popup-padding)',
+      'px-0 py-(--menu-popup-padding) bitrix-mobile:py-0!',
       'pointer-events-auto'
     ].join(' '),
     viewport: [
       'relative',
       'w-full max-h-[40vh] min-w-[192px]',
-      'overflow-x-hidden overflow-y-auto scrollbar-thin' // scrollbar-transparent
+      'overflow-x-hidden overflow-y-auto scrollbar-thin'
     ].join(' '),
-    arrow: 'fill-(--ui-color-bg-content-primary)', // stroke-(--ui-color-divider-default) for content bottom|top::start -> ml-[12px]
-    group: 'grid', // p-1 isolate
+    arrow: 'fill-(--ui-color-bg-content-primary)',
+    group: 'grid',
     label: [
-      'w-full h-(--popup-window-delimiter-section-height)', // min-w-[195px]
+      'w-full h-(--popup-window-delimiter-section-height)',
+      'bitrix-mobile:h-[27px]',
       'px-[18px] mt-(--menu-item-block-stack-space)',
+      'bitrix-mobile:pe-0 bitrix-mobile:mt-0 bitrix-mobile:-mb-[12px]',
       'flex flex-row rtl:flex-row-reverse items-center',
       'select-none outline-none whitespace-nowrap',
       'text-start',
       'text-(length:--ui-size-sm)',
-      'text-(--b24ui-typography-legend-color)',
+      'text-(--ui-color-base-3)',
+      'bitrix-mobile:[&_[data-slot="itemLabel"]]:text-(--ui-color-base-5)',
       'font-(--ui-font-weight-normal)',
       'after:ms-[10px] after:block after:flex-1 after:min-w-[15px] after:h-px after:bg-(--ui-color-divider-vibrant-default)'
     ].join(' '),
-    separator: 'my-[8px] mx-[18px] h-[1px] bg-(--ui-color-divider-vibrant-default)',
+    separator: [
+      'bg-(--ui-color-divider-vibrant-default)',
+      'my-2 mx-4.5 h-px',
+      'bitrix-mobile:my-0 bitrix-mobile:mx-0 bitrix-mobile:h-1.5'
+    ].join(' '),
     item: [
       'group',
-      'w-full h-[36px]', // min-w-[195px]
-      'px-[18px] mt-(--menu-item-block-stack-space)',
+      'w-full h-[36px]',
+      'bitrix-mobile:h-[42px]',
+      'px-4.5 pe-2 mt-(--menu-item-block-stack-space)',
+      'bitrix-mobile:mt-0',
       'relative',
-      'flex flex-row rtl:flex-row-reverse items-center',
+      'flex flex-row rtl:flex-row-reverse items-center gap-2',
       'select-none outline-none whitespace-nowrap',
       'cursor-pointer',
       'data-disabled:cursor-not-allowed data-disabled:opacity-30',
       'text-start',
       'text-(length:--ui-font-size-md)',
-      'text-(--b24ui-typography-legend-color) hover:text-(--b24ui-typography-label-color)',
-      'data-highlighted:text-(--b24ui-typography-label-color)',
-      'data-[state=open]:text-(--b24ui-typography-label-color)',
+      'text-(--ui-color-base-1)',
+      'hover:text-(--ui-color-base-1)',
+      'data-highlighted:text-(--ui-color-base-1)',
+      'data-[state=open]:text-(--ui-color-base-1)',
       'hover:bg-(--ui-color-divider-optical-1-alt)',
       'data-highlighted:bg-(--ui-color-divider-optical-1-alt)',
       'data-[state=open]:bg-(--ui-color-divider-optical-1-alt)',
-      'transition-colors'
+      'bitrix-mobile:data-[state=open]:bg-(--ui-color-bg-state-hover-default)',
+      'transition-colors',
+      'bitrix-mobile:[[data-slot="item"]+&]:border-t',
+      'bitrix-mobile:border-t-(--ui-color-divider-vibrant-default)',
+      'bitrix-mobile:data-highlighted:bg-(--ui-color-bg-state-hover-default)'
     ].join(' '),
     itemLeadingIcon: [
       'shrink-0',
-      'size-[18px]',
-      'text-(--ui-color-design-plain-content-icon-secondary)',
+      'size-[25px]',
+      'text-(--ui-color-base-5)',
       'group-data-highlighted:text-(--ui-color-accent-main-primary)',
       'group-data-[state=open]:text-(--ui-color-accent-main-primary)',
       'group-data-[state=checked]:text-(--ui-color-accent-main-primary)',
@@ -73,7 +85,7 @@ export default {
     itemLeadingAvatar: 'shrink-0 size-[16px] me-[8px]', // @memo 18-2px
     itemLeadingAvatarSize: '2xs', // @memo this wrong
     itemTrailing: 'ml-auto rtl:ml-0 rtl:mr-auto inline-flex gap-1.5 items-center',
-    itemTrailingIcon: 'shrink-0 size-[24px] text-(--ui-color-accent-main-primary)',
+    itemTrailingIcon: 'shrink-0 size-[25px] text-(--ui-color-accent-main-primary)',
     itemTrailingKbds: 'shrink-0 hidden lg:inline-flex items-center gap-0.5',
     itemTrailingKbdsSize: 'md',
     itemWrapper: 'flex-1 flex flex-col text-start min-w-0',
@@ -82,7 +94,7 @@ export default {
       'group-data-[state=checked]:text-(--ui-color-accent-main-primary)'
     ].join(' '),
     itemDescription: 'max-w-[240px] truncate -mt-[6px] text-(--b24ui-typography-description-color) text-(length:--ui-font-size-sm)',
-    itemLabelExternalIcon: 'inline-block size-[16px] text-(--ui-color-design-plain-content-icon-secondary)'
+    itemLabelExternalIcon: 'inline-block size-[25px] text-(--ui-color-base-5)'
   },
   variants: {
     color: {
@@ -91,7 +103,7 @@ export default {
       'air-primary-alert': { item: 'style-filled-alert' },
       'air-primary-copilot': { item: 'style-filled-copilot' },
       'air-primary-warning': { item: 'style-filled-warning' },
-      // @deprecate ////
+      /* @deprecate This rule is deprecated and will be removed in version 3.0.0 */
       'default': { item: 'style-old-default' },
       'danger': { item: 'style-old-danger' },
       'success': { item: 'style-old-success' },
@@ -156,6 +168,7 @@ export default {
       active: true,
       class: ''
     },
+    /* @deprecate This rule is deprecated and will be removed in version 3.0.0 */
     {
       color: ['danger', 'success', 'warning', 'primary', 'secondary', 'collab', 'ai'],
       active: false,
@@ -168,6 +181,7 @@ export default {
         ].join(' ')
       }
     },
+    /* @deprecate This rule is deprecated and will be removed in version 3.0.0 */
     {
       color: ['danger', 'success', 'warning', 'primary', 'secondary', 'collab', 'ai'],
       active: true,
