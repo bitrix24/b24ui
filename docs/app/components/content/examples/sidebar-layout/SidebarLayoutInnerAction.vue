ca5314ec72c0b489a2fe75469b031e6be25e79cd
Shevchik Igor <s.hevchik.mail@gmail.com>
2026-03-24 16:04:58 +0300

    docs(SidebarLayout): deprecated

--- docs/app/components/content/examples/sidebar-layout/SidebarLayoutInnerAction.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-const handleActionByContext = async () => {
-  await new Promise(resolve => setTimeout(resolve, 2_000))
-}
-</script>
-
-<template>
-  <B24Button label="Action" color="air-primary" loading-auto @click="handleActionByContext" />
-</template>
