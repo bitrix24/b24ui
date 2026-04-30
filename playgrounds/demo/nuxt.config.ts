df826dcf28918831ad26f5a7626dd7f3961f7d89
Shevchik Igor <s.hevchik.mail@gmail.com>
2026-04-30 07:00:12 +0300

    playgrounds(nuxt): add vite `optimizeDeps.include`

--- playgrounds/demo/nuxt.config.ts
+++ playgrounds/demo/nuxt.config.ts
@@ -40,6 +40,69 @@ export default defineNuxtConfig({
 
   compatibilityDate: '2024-07-09',
 
+  vite: {
+    server: {
+      // Fix: "Blocked request. This host is not allowed" when using tunnels like ngrok
+      // allowedHosts: [...extraAllowedHosts]
+    },
+    optimizeDeps: {
+      include: [
+        '@vueuse/core',
+        'tailwindcss/colors',
+        'vaul-vue',
+        'tailwind-variants',
+        '@vueuse/integrations/useFuse',
+        '@bitrix24/b24icons-vue/outline/HamburgerMenuIcon',
+        '@bitrix24/b24icons-vue/main/B24Icon',
+        '@bitrix24/b24icons-vue/actions/ArrowToTheLeftIcon',
+        '@bitrix24/b24icons-vue/actions/ArrowToTheRightIcon',
+        '@bitrix24/b24icons-vue/outline/CheckLIcon',
+        '@bitrix24/b24icons-vue/outline/ChevronTopLIcon',
+        '@bitrix24/b24icons-vue/outline/ChevronLeftLIcon',
+        '@bitrix24/b24icons-vue/outline/ChevronRightLIcon',
+        '@bitrix24/b24icons-vue/actions/DoubleShevronsRightIcon',
+        '@bitrix24/b24icons-vue/actions/DoubleShevronsLeftIcon',
+        '@bitrix24/b24icons-vue/outline/CrossMIcon',
+        '@bitrix24/b24icons-vue/button/DotsIcon',
+        '@bitrix24/b24icons-vue/actions/Refresh6Icon',
+        '@bitrix24/b24icons-vue/actions/Minus30Icon',
+        '@bitrix24/b24icons-vue/actions/Plus30Icon',
+        '@bitrix24/b24icons-vue/main/Search2Icon',
+        '@bitrix24/b24icons-vue/animated/LoaderWaitIcon',
+        '@bitrix24/b24icons-vue/outline/ChevronDownLIcon',
+        '@bitrix24/b24icons-vue/outline/ScreenIcon',
+        '@bitrix24/b24icons-vue/outline/SunIcon',
+        '@bitrix24/b24icons-vue/outline/MoonIcon',
+        '@bitrix24/b24icons-vue/outline/TagIcon',
+        '@bitrix24/b24icons-vue/outline/InfoCircleIcon',
+        '@bitrix24/b24icons-vue/outline/IdeaLampIcon',
+        '@bitrix24/b24icons-vue/main/WarningIcon',
+        '@bitrix24/b24icons-vue/outline/AlertIcon',
+        '@bitrix24/b24icons-vue/outline/CopyIcon',
+        '@bitrix24/b24icons-vue/outline/CircleCheckIcon',
+        '@bitrix24/b24icons-vue/outline/FileIcon',
+        '@bitrix24/b24icons-vue/outline/UploadFileIcon',
+        '@bitrix24/b24icons-vue/outline/ArrowDownLIcon',
+        '@bitrix24/b24icons-vue/outline/ArrowTopLIcon',
+        '@bitrix24/b24icons-vue/outline/StopLIcon',
+        '@bitrix24/b24icons-vue/outline/RefreshIcon',
+        '@bitrix24/b24icons-vue/main/SendIcon',
+        '@bitrix24/b24icons-vue/outline/DragLIcon',
+        '@bitrix24/b24icons-vue/outline/GoToLIcon',
+        '@bitrix24/b24icons-vue/outline/CloseChatIcon',
+        '@bitrix24/b24icons-vue/outline/OpenChatIcon',
+        '@bitrix24/b24icons-vue/outline/ChevronDownSIcon',
+        '@bitrix24/b24icons-vue/animated/LoaderClockIcon',
+        '@bitrix24/b24icons-vue/specialized/SpinnerIcon',
+        '@bitrix24/b24icons-vue/actions/Cross20Icon',
+        '@bitrix24/b24icons-vue/crm/ItemIcon',
+        '@bitrix24/b24icons-vue/outline/TextIcon',
+        '@bitrix24/b24icons-vue/outline/HomeIcon',
+        '@bitrix24/b24icons-vue/outline/CopilotIcon'
+      ]
+    }
+  },
+
   nitro: {
     output: {
       publicDir: '.output/public/b24ui/demo'
