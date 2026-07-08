10e73b9b98158c32d06e113a3c7c9f87a29fe586
Shevchik Igor <s.hevchik.mail@gmail.com>
2026-07-08 10:55:02 +0300

    feat(Calendar): add month and year selection

---
+++ docs/app/components/content/examples/calendar/CalendarTodayExample.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { getLocalTimeZone, today } from '@internationalized/date'
+
+const date = shallowRef(today(getLocalTimeZone()))
+</script>
+
+<template>
+  <div class="flex flex-col gap-4">
+    <B24Calendar v-model="date" />
+
+    <B24Button color="air-primary" class="justify-center" @click="date = today(getLocalTimeZone())">
+      Today
+    </B24Button>
+  </div>
+</template>
